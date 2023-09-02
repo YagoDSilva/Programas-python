0548f385cb9a50d91dc23d53cfef465460c0a05e
YagoDSilva <yagovill@silva23@gmail.com>
2023-09-02 09:20:40 -0300

    UpdateP_02_09_2023

--- Exercio_2.py
+++ Exercio_2.py
@@ -4,7 +4,7 @@ horas_trabalhadas_mes= float(input("Horas trabalhadas/mês "))
 
 salario_total = salario_hora * horas_trabalhadas_mes
 
-print(f"ganhando R$ {salario_hora:.2f} a hora, e trabalhando "f"{horas_trabalhadas_mes:.2f} horas por mês, no final do mês ganhará "f"{salario_total:.2f} por mês")
+print(f"ganhando R$ {salario_hora:.2f} a hora, e trabalhando "f"{horas_trabalhadas_mes:.2f} horas por mês, no final do mês ganhará"f"{salario_total:.2f} por mês")
 # texto = "Olá mundo";
 # partes = texto.split()
 #             0       1
---
+++ Sla.py
@@ -0,0 +1,19 @@
+from threading import Event
+delay = 1
+b = 0
+c = 0
+while(c <=9):
+    while(b <=9):
+        Event().wait(delay)
+        b = b+1
+        print(b)
+    while(b > 9):
+        Event().wait(delay)
+        b = b-1
+        c = c+1
+        print(f"c ={c}")
+        print(b)
+        
+        
+    
+    
